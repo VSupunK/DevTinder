51ee02d4479d229084248a44dbe6dd6412dbd46f
Supun Kalhara <116533030+VSupunK@users.noreply.github.com>
2024-12-19 10:42:58 +0530

    Add admin data retrieval endpoint

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,21 +1,33 @@
-const express = require('express');
+const express = require("express");
 
-const app = express(); // create express app 
+const app = express(); // create express app
 
 // this will only handle GET requests to /user
-app.get('/user/:userId/:name/:password', (req, res) => {
-    console.log(req.params);
-    res.send({
-        "fistName": "John",
-        "lastName": "Doe",
-    })
-})
+app.get("/user/:userId/:name/:password", (req, res) => {
+  console.log(req.params);
+  res.send({
+    fistName: "John",
+    lastName: "Doe",
+  });
+});
 
 //this will match all the HTTP methods API calls to /test
-app.use('/test', (req, res) => {
-    res.send("Hello World");
-})
+app.use("/test", (req, res) => {
+  res.send("Hello World");
+});
+
+app.get("/admin/getAllData", (req, res) => {
+  //Logic of checking if the request is authorized
+  const token = "xyz123";
+  isAdminAuthorized = token === "xyz123";
+
+  if (isAdminAuthorized) {
+    res.send("All Data sent to Admin");
+  } else {
+    res.status(401).send("Unauthorized Access");
+  }
+});
 
 app.listen(7777, () => {
-    console.log('Server is running on port 7777');
+  console.log("Server is running on port 7777");
 });
